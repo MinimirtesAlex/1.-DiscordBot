d94c1104ea40e62e144cefd614328cd26044d415
Alex <alex@guhlonline.de>
2021-03-10 11:00:13 +0100

    14th

--- index.js
+++ index.js
@@ -14,56 +14,57 @@ bot.on('message', message =>{
     
     let args = message.content.substring(PREFIX.length).split(" ");
 
-    let test = message.content.startsWith(PREFIX).split(" ");
+    let lowercaseargs = args.map(args => args.toLowerCase());
 
-    let lowercaseargs = test.map(args => test.toLowerCase());
+    if(args[0].startsWith(PREFIX)) {
 
-    switch (lowercaseargs[0]){
+        switch (lowercaseargs[0]){
 
-        case 'help':
-            message.channel.send('Hallo ich bin hier der BABO!\n Mein Prefix ist "' + PREFIX + '"!\n Meine Befehle sind:\n - **ping** | wir spielen ein schönes Spiel\n - **clear** [1 - 100] | Aus dem Chanel werden [1 -100] Nachrichten gelöscht\n Das wars. Wenn du noch wünsche hast, dann schreib Alex!')
-        break;
+            case 'help':
+                message.channel.send('Hallo ich bin hier der BABO!\n Mein Prefix ist "' + PREFIX + '"!\n Meine Befehle sind:\n - **ping** | wir spielen ein schönes Spiel\n - **clear** [1 - 100] | Aus dem Chanel werden [1 -100] Nachrichten gelöscht\n Das wars. Wenn du noch wünsche hast, dann schreib Alex!')
+            break;
 
-        case 'ping':
-            message.channel.send('Pong!')
-        break;
+            case 'ping':
+                message.channel.send('Pong!')
+            break;
 
-        case 'clear':
-            if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('Dir fehlt die benötigte Berechtigung! (MANAGE_MESSAGES)')
-            if(!args[1]) return message.channel.send('Fehler, bitte gib eine Zahl zwischen 2 und 99 als zweiten Parameter an!')
-            //if(lowercaseargs[1].includes('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ä', 'ö', 'ü', 'ß')) return message.channel.send('Fehler, bitte gib nur Zahlen an!')
-            //if(args[1].includes('!', '"', '§', '$', '%', '&', '/', '(', '{', '[', ']', '}', ')', '=', '?', '`', '´', '*', '+', '~', '#', '-', '_', '.', ':', ',', ';', 'µ', '<', '>', '|', '^', '°', '@', '€')) return message.channel.send('Fehler, bitte gib nur Zahlen an!')
-            if(args[1].match('all')) {
-                message.channel.bulkDelete(100)
-                message.channel.bulkDelete(100)
-                message.channel.bulkDelete(100)
-                message.channel.bulkDelete(100)
-                message.channel.bulkDelete(100)
-                message.channel.bulkDelete(100)
-                message.channel.bulkDelete(100)
-                message.channel.bulkDelete(100)
-                message.channel.bulkDelete(100)
+            case 'clear':
+                if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('Dir fehlt die benötigte Berechtigung! (MANAGE_MESSAGES)')
+                if(!args[1]) return message.channel.send('Fehler, bitte gib eine Zahl zwischen 2 und 99 als zweiten Parameter an!')
+                //if(lowercaseargs[1].includes('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ä', 'ö', 'ü', 'ß')) return message.channel.send('Fehler, bitte gib nur Zahlen an!')
+                //if(args[1].includes('!', '"', '§', '$', '%', '&', '/', '(', '{', '[', ']', '}', ')', '=', '?', '`', '´', '*', '+', '~', '#', '-', '_', '.', ':', ',', ';', 'µ', '<', '>', '|', '^', '°', '@', '€')) return message.channel.send('Fehler, bitte gib nur Zahlen an!')
+                if(args[1].match('all')) {
+                    message.channel.bulkDelete(100)
+                    message.channel.bulkDelete(100)
+                    message.channel.bulkDelete(100)
+                    message.channel.bulkDelete(100)
+                    message.channel.bulkDelete(100)
+                    message.channel.bulkDelete(100)
+                    message.channel.bulkDelete(100)
+                    message.channel.bulkDelete(100)
+                    message.channel.bulkDelete(100)
 
-            }
-            message.channel.bulkDelete(args[1])
-        break;
+                }
+                message.channel.bulkDelete(args[1])
+            break;
 
-        case 'test':
-            message.channel.send('Works')
-            if(message.member.hasPermission('ADMINISTRATOR')) {
-                message.channel.send('Test 1')
-                message.channel.send('Test 2')
-            }
-            else {
-                message.channel.send('Test 3')
-            }
-        break;
+            case 'test':
+                message.channel.send('Works')
+                if(message.member.hasPermission('ADMINISTRATOR')) {
+                    message.channel.send('Test 1')
+                    message.channel.send('Test 2')
+                }
+                else {
+                    message.channel.send('Test 3')
+                }
+            break;
 
-        default:
-            message.channel.send('Fehler, dieser Command existiert noch nicht! Bitte Alex ihn hinzuzufügen.')
-        break;
+            default:
+                message.channel.send('Fehler, dieser Command existiert noch nicht! Bitte Alex ihn hinzuzufügen.')
+            break;
 
 
+        }
     }
 })
 
