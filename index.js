c241e69ea5230ad1febc87ddf282c7e775102392
Alex <alex@guhlonline.de>
2021-03-10 08:49:31 +0100

    1st
    The Foundation

---
+++ index.js
@@ -0,0 +1,42 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const PREFIX = '.';
+const TOKEN = 'ODE4ODI4MDY5NTczMjMwNjEz.YEdvEA.6_u3HUbKpBjWbRLURSye3p2Wdbg';
+
+var version = '1.0.1';
+
+bot.on('ready', () => {
+    console.log('Der Bot ist nun Online!')
+
+    bot.user.setActivity('.help', { type: 'PLAYING'}).catch(console.error)
+})
+
+bot.on('message', message =>{
+    
+    let args = message.content.substring(PREFIX.length).split(" ");
+
+    let lowercaseargs = args.map(args => args.toLowerCase());
+
+    switch (lowercaseargs[0]){
+
+        case 'help':
+            message.channel.send('Hallo ich bin hier der BABO!\n Mein Prefix ist "' + PREFIX + '"!\n Meine Befehle sind:\n - **ping** | wir spielen ein schönes Spiel\n - **clear** [1 - 100] | Aus dem Chanel werden [1 -100] Nachrichten gelöscht\n Das wars. Wenn du noch wünsche hast, dann schreib Alex!')
+        break;
+
+        case 'ping':
+            message.channel.send('Pong!')
+        break;
+
+        case 'clear':
+            if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('Dir fehlt die benötigte Berechtigung! (MANAGE_MESSAGES)')
+            if(!args[1]) return message.channel.send('Fehler, bitte gib eine Zahl zwischen 1 und 99 als zweiten Parameter an!')
+            if(lowercaseargs[1].includes('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ä', 'ö', 'ü', 'ß')) return message.channel.send('Fehler, bitte gib nur Zahlen an!')
+            message.channel.bulkDelete(args[1])
+        break;
+
+
+    }
+})
+
+
+bot.login(TOKEN)
